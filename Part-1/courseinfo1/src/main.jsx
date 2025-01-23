861cc11dc0235bedd081ef7fa87d3bd1c662cd7d
pasang99 <pasangl046@gmail.com>
2025-01-23 21:59:18 +0545

    part-1-exercise completed-resubmit

---
+++ Part-1/courseinfo1/src/main.jsx
@@ -0,0 +1,3 @@
+import ReactDOM from 'react-dom/client'
+import App from "./App"; // Correct name of the file
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
---
+++ Part-1/unicafe1/src/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from "./App"; // Correct name of the file
+ReactDom.createRoot(document.getElementById('root')).render(<App />);
