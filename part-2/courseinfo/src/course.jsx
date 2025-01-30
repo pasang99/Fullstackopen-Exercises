dc84616eda2bbd86d762c8bc2aa7bcdbfe0e91b5
pasang99 <pasangl046@gmail.com>
2025-01-30 11:45:39 +0545

    sum of the exercises-part-2

--- part-2/courseinfo/src/course.jsx
+++ part-2/courseinfo/src/course.jsx
@@ -2,10 +2,14 @@ import Header from "./Header";
 import Content from "./Content";
 
 const Course = ({ course }) => {
+  // Calculate total exercises
+  const totalExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <p><strong>Total exercises: {totalExercises}</strong></p> {/* Show total */}
     </div>
   );
 };
