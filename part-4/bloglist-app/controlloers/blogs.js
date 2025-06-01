8fb5fcceb86588e193bae5e523179d87192dbf1d
pasang99 <pasangl046@gmail.com>
2025-06-01 12:06:32 +0545

    excercise part-4,4.1-4.2 done

---
+++ part-4/bloglist-app/controlloers/blogs.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const Blog = require('../models/blog')
+
+const blogsRouter = express.Router()
+
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.json(blogs)
+})
+
+blogsRouter.post('/', async (req, res) => {
+  const blog = new Blog(req.body)
+  const result = await blog.save()
+  res.status(201).json(result)
+})
+
+module.exports = blogsRouter
